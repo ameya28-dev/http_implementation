15ccda009b91238febd9d57f3c2587d7ea035606
Ameya G Mahale <ameya28.mahale@outlook.com>
2025-01-22 23:21:57 +0530

    Future waits

--- main.dart
+++ main.dart
@@ -6,9 +6,51 @@ import 'model/get/response.dart';
 import 'model/post/response.dart';
 import 'utils/urls.dart';
 
-Future<void> main() async {
+void main()  {
   LoggingUtils.initialize();
+  future1().then((res) => print('Time 1: $res'));
+  future2().then((res) => print('Time 2: $res'));
+}
 
+Future<Duration> future2() async {
+  Stopwatch stopwatch = Stopwatch();
+  stopwatch.start();
+  final result = await Future.wait(
+    [
+      HttpMethod.instance.httpGetList(
+        authority: BaseUrl.restful,
+        path: '/${Endpoints.objects}blmkdslvms ',
+        queryParameters: {
+          'id': ['3', '5', '4']
+        },
+        responseBuilder: (json) => json.map((v) => GetResponse()..fromJson(v)).toList(),
+      ),
+      HttpMethod.instance.httpPost(
+        url: 'https://${BaseUrl.restful}/${Endpoints.objects}',
+        request: req.PostRequest(
+          name: 'Apple MacBook Pro 16',
+          data: req.Data(
+            year: 2019,
+            price: 1499.9,
+            cPUModel: 'Intel iCore 9',
+            hardDiskSize: '1 TB',
+          ),
+        ),
+        responseBuilder: (json) => PostResponse()..fromJson(json),
+      ),
+    ],
+  );
+
+  for (var r in result) {
+    LoggingUtils.info(jsonEncode(r));
+  }
+  stopwatch.stop();
+  return stopwatch.elapsed;
+}
+
+Future<Duration> future1() async {
+  Stopwatch stopwatch = Stopwatch();
+  stopwatch.start();
   List<GetResponse> res = await HttpMethod.instance.httpGetList(
     authority: BaseUrl.restful,
     path: '/${Endpoints.objects}blmkdslvms ',
@@ -35,4 +77,6 @@ Future<void> main() async {
   );
 
   LoggingUtils.info(jsonEncode(response));
+  stopwatch.stop();
+  return stopwatch.elapsed;
 }
